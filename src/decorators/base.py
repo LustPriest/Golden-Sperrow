72f973c14d5871c9619148a7056f69ac8e9d83f2
Divu <deadlydemon302@gmail.com>
2024-09-28 22:22:45 +0530

    Update and rename handlers.py to base.py

---
+++ src/decorators/base.py
@@ -0,0 +1,65 @@
+from typing import Optional, List, Tuple, Union, Pattern
+
+from telegram.ext import filters, CallbackQueryHandler, CommandHandler, MessageHandler
+from telegram.ext.filters import BaseFilter
+
+
+def command(
+    command: Union[List[str], Tuple[str], str],
+    filters: Optional[BaseFilter] = None,
+    block: Optional[bool] = True,
+    has_args: Optional[Union[bool, int]] = None,
+    group: Optional[int] = 0,
+):
+    async def wrapper(callback):
+        await application.add_handler(
+            CommandHandler(
+                command,
+                callback,
+                filters,
+                block,
+                has_args
+            ),
+            group
+        )
+        logger.debug(
+            'Loaded handler %s for function %s',
+            command,
+            callback.__name__
+        )
+        return callback
+    return wrapper
+
+
+def message(
+    filters: Optional[BaseFilter] = None,
+    block: Optional[bool] = True,
+    group: Optional[int] = 0,
+):
+    async def wrapper(callback):
+        await application.add_handler(MessageHandler(filters, callback, block), group)
+        logger.debug(
+            'Loaded filter pattern %s for function %s',
+            pattern,
+            callback.__name__
+        )
+        return callback
+    return wrapper
+
+
+def callback_query(
+    pattern: Optional[
+        Union[str, Pattern[str], Optional[bool]]
+    ] = None,
+    block: Optional[bool] = True,
+    group: Optional[int] = 0,
+):
+    async def wrapper(callback):
+        await application.add_handler(CallbackQueryHandler(pattern=pattern, callback=callback, block=block), group)
+        logger.debug(
+            'Loaded callbackquery handler with pattern %s for function %s',
+            pattern,
+            callback.__name__
+        )
+        return callback
+    return wrapper
