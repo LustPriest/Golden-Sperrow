9ac95276e3ebc15cfedb51c6c56f0cb82facb7d1
Divu <deadlydemon302@gmail.com>
2024-09-28 20:11:13 +0530

    Create constants.py

---
+++ src/constants.py
@@ -0,0 +1,21 @@
+import os
+from pathlib import Path
+
+BOT_ROOT_PATH: Path = Path(__file__).parent.parent.parent
+
+XDG_CONFIG_HOME: str = str(Path.home() / ".config")
+DEFAULT_CONFIG_PATH: str = f"{XDG_CONFIG_HOME}/configuration.toml"
+
+DEFAULT_CONFIG_TEMPLATE: dict[str, dict[str, str | bool | int] | dict[str, list[str] | str]] = {
+    "telegram": {
+        "TOKEN": "",
+        "SUDOERS": ["123"],
+        "LOGS_CHAT": "-123",
+        "BACKUPS_CHAT": "-123",
+    },
+    "general": {
+        "SENTRY_DSN": "",
+    },
+}
+
+MESSAGE_LENGTH_LIMIT: int = 4096
