e9d1afba192f3c4e851495994853b6a642b3d2aa
Divu <deadlydemon302@gmail.com>
2024-09-29 18:53:37 +0530

    Update caching.py

--- src/utils /caching.py
+++ src/utils /caching.py
@@ -1,4 +1,14 @@
-from cashews import Cache
+from cashews import Cache 
+from .utils.logging import logger
+import asyncio
 
 cache = Cache()
 cache.setup('redis://localhost', client_side=True)
+
+async def pre_process() -> ConfigManager:
+    try:
+        await cache.ping()
+    except (CacheBackendInteractionError, TimeoutError):
+        await logger.acritical("Can't connect to RedisDB! Exiting...")
+
+asyncio.run(pre_process())
