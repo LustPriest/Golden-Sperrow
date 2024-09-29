36613ae2a720aa9561685c32ecd7e09ff62f10fb
Divu <deadlydemon302@gmail.com>
2024-09-29 13:48:13 +0530

    Update start.py

--- src/modules/start.py
+++ src/modules/start.py
@@ -2,3 +2,10 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.constants import ParseMode
 from telegram.ext import CallbackContext, CallbackQueryHandler
 
+from ..decorators import router
+
+
+@router.command('start', pass_args=True)
+@rate_limit(1, 2)
+async def start(update: Update, context: CallbackContext):
+    await update.effective_message.reply_text('Hmmm?')
