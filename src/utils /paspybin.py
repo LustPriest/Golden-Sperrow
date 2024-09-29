2dff648af8e16eff343415a21b8c48ae93ca64f2
Divu <deadlydemon302@gmail.com>
2024-09-29 16:43:27 +0530

    Update paspybin.py

--- src/utils /paspybin.py
+++ src/utils /paspybin.py
@@ -3,7 +3,7 @@ from paspybin.exceptions import PaspybinBadAPIRequestError
 
 from src.config import ConfigManager
 
-PASTEBIN_API_DEV_KEY = ConfigManager.get('api', 'PASTEBIN_API_DEV_KEY')
+PASTEBIN_API_DEV_KEY = ConfigManager.get('general', 'PASTEBIN_API_DEV_KEY')
 
 async def paste(content: str) -> str:
     async with Paspybin(PASTEBIN_API_DEV_KEY) as paspybin:
