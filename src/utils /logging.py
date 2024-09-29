66131b0dfc46babcf62ed1468ab3ca37fc928a42
Divu <deadlydemon302@gmail.com>
2024-09-29 09:44:27 +0530

    Create logging.py

---
+++ src/utils /logging.py
@@ -0,0 +1,22 @@
+import sys
+
+import picologging as logging
+import structlog
+
+structlog.configure(
+    cache_logger_on_first_use=True,
+    processors=[
+        structlog.contextvars.merge_contextvars,
+        structlog.processors.add_log_level,
+        structlog.processors.TimeStamper(fmt="%d-%m-%Y %H:%M.%S", utc=False),
+        structlog.dev.ConsoleRenderer(),
+    ],
+)
+
+logging.basicConfig(
+    format="%(message)s",
+    stream=sys.stdout,
+    level=logging.INFO,
+)
+
+logger = structlog.wrap_logger(logging.getLogger("Golden Sperrow"))
