3c45571f59fb7499da249fb3677a4166a759baf5
Divu <deadlydemon302@gmail.com>
2024-09-29 13:44:40 +0530

    router.py: added *rate limit*

--- src/decorators/router.py
+++ src/decorators/router.py
@@ -1,10 +1,32 @@
+from functools import wraps
 from typing import Optional, List, Tuple, Union, Pattern
 
-from telegram.ext import filters, CallbackQueryHandler, CommandHandler, MessageHandler
-from telegram.ext.filters import BaseFilter
+from telegram import Update
+from telegram.ext import CallbackContext, CommandHandler, CallbackQueryHandler, MessageHandler
+from telegram.ext.filters import BaseFilter, UpdateType
 
 from ..application import application
 from ..utils.logging import logger
+from ..utils.cache import cache
+
+
+def rate_limit(messages_per_window: int, window_seconds: int):
+    def decorator(callback):
+        @wraps(callback)
+        async def wrapper(update: Update, context: CallbackContext):
+            key = f'{update.effective_user.id}'
+            try:
+                await cache.rate_limit(key, limit=messages_per_window, period=window_seconds)
+            except cache.RateLimitError:
+                await logging.ainfo(
+                    'Rate limit exceeded for user %s. '
+                    'Allowed %s updates in %s seconds.',
+                    key, messages_per_window, window_seconds
+                )
+                return
+            return await callback(update, context)
+        return wrapper
+    return decorator
 
 
 def command(
@@ -15,6 +37,8 @@ def command(
     group: Optional[int] = 0
 ):
     async def wrapper(callback):
+        if filters is not None: filters = ~UpdateType.EDITED & filters
+        else: filters = ~UpdateType.EDITED
         await application.add_handler(
             CommandHandler(
                 command,
@@ -40,10 +64,11 @@ def message(
     group: Optional[int] = 0
 ):
     async def wrapper(callback):
+        if filters is not None: filters = ~UpdateType.EDITED & filters
+        else: filters = ~UpdateType.EDITED
         await application.add_handler(MessageHandler(filters, callback, block), group)
         await logger.adebug(
-            'Loaded filter pattern %s for function %s',
-            pattern,
+            'Loaded filter pattern for function %s',
             callback.__name__
         )
         return callback
